d304e043066cbb4cdfb421ee3de8ba011e8d79af
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-04-25 13:33:43 -0400

    dynamic posts and lifting states

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,19 +1,45 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import classes from './NewPost.module.css'
 
-export default function NewPost(props) {
+export default function NewPost({onCancel, onAddPost}) { // example of props destructuring
+  const [enteredBody, setEnteredBody] = useState('')
+  const [enteredAuthor, setEnteredAuthor ] = useState('')
+
+  
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value)
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value)
+  }
+
+  function submitHandler(event) {
+    event.preventDefault() // prevents the default function of the submit button which is submitting an http request to the server
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    }
+    console.log(postData)
+    onAddPost(postData)
+    onCancel()
+  }
 
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label 
           htmlFor='body'>Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler}/>
       </p>
       <p>
         <label 
         htmlFor='name'>Your Name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange}/>
+        <input type="text" id="name" required onChange={authorChangeHandler}/>
+      </p>
+      <p className={classes.actions}>
+        <button type="button" onClick={onCancel}>Cancel</button>
+        <button>Submit</button>
       </p>
     </form>
   )
--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -6,16 +6,12 @@ import Modal from './Modal'
 import classes from './PostsList.module.css'
 
 export default function PostsList({isPosting, onStopPosting}) { // example of destructured props
-  const [enteredBody, setEnteredBody] = useState('')
-  const [enteredAuthor, setEnteredAuthor ] = useState('')
+  const [posts, setPosts] = useState([]) // used for dynamic rendering of Posts component
 
-  
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value)
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value)
+  function addPostHandler(postData) {
+    // setPosts([postData, ...posts]) //...spread operator, used to add to existing array, in this case, posts
+    // updating arrays is not ideal because you are updating state. if new state depends on old state, use a function instead
+    setPosts((existingPosts) => [postData, ...existingPosts])
   }
 
   // let modalContent;
@@ -32,17 +28,27 @@ export default function PostsList({isPosting, onStopPosting}) { // example of de
 
   return(
     <>
-    {isPosting ?  
+    {isPosting && (  
       <Modal onClose={onStopPosting}>
         <NewPost 
-          onBodyChange={bodyChangeHandler} 
-          onAuthorChange={authorChangeHandler}
+          // onBodyChange={bodyChangeHandler} 
+          // onAuthorChange={authorChangeHandler}
+          onCancel={onStopPosting}
+          onAddPost={addPostHandler}
         />
-      </Modal> : null}
+     </Modal>
+    )}
+    {posts.length > 0 && (
       <ul className={classes.posts}>
-      <Post author={enteredAuthor} body={enteredBody}/>
-      <Post author="Meelyn" body="Learning React JS is fun!"/>
+        {posts.map((post) => <Post key= {post.body} author={post.author} body={post.body} />)} {/*Turning elements in posts array into Post component */}
+      {/* <Post author={enteredAuthor} body={enteredBody}/> */}
+      {/* <Post author="Meelyn" body="Learning React JS is fun!"/> */}
       </ul>
+    )}
+    {posts.length === 0 && <div style={{textAlign: 'center', color: 'black'}}>
+      <h2>There are no posts yet.</h2>
+      <p>Start adding some!</p>
+      </div>}
     </>
   )
 }
