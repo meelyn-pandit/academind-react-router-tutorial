679ead42085342943d4e89bea2cb17245897726b
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-04-25 10:41:05 -0400

    child props, state passing, and updated new post button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useState } from 'react'
 import './App.css';
 // import Post from './components/Post'
 import PostsList from './components/PostsList'
+import MainHeader from './components/MainHeader'
 
 function App() { // must wrap jsx in a single html component (div, main, etc.)
+  const [ modalIsVisible, setModalIsVisible ] = useState(false)
+
+  function showModalHandler() {
+    setModalIsVisible(true)
+  }
+  function hideModalHandler() {
+    setModalIsVisible(false)
+  }
+  
+
   return (
-    <main>
-      <PostsList/>
+    <>
+    <MainHeader onCreatePost={showModalHandler}/>
+      <main>
+      <PostsList isPosting={modalIsVisible}
+        onStopPosting={hideModalHandler}/>
     </main>
+    </>
+    
   );
 }
 
--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -2,11 +2,13 @@ import { useState } from 'react'
 
 import Post from './Post'
 import NewPost from './NewPost'
+import Modal from './Modal'
 import classes from './PostsList.module.css'
 
-export default function PostsList(props) {
+export default function PostsList({isPosting, onStopPosting}) { // example of destructured props
   const [enteredBody, setEnteredBody] = useState('')
   const [enteredAuthor, setEnteredAuthor ] = useState('')
+
   
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value)
@@ -16,13 +18,31 @@ export default function PostsList(props) {
     setEnteredAuthor(event.target.value)
   }
 
+  // let modalContent;
+
+  // if (modalIsVisible) {
+  //   modalContent = (<Modal onClose={hideModalHandler}>
+  //     <NewPost 
+  //       onBodyChange={bodyChangeHandler}
+  //       onAuthorChange={authorChangeHandler}
+  //     />
+  //   </Modal>
+  //   )
+  // }
+
   return(
     <>
-    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
-    <ul className={classes.posts}>
-    <Post author={enteredAuthor} body={enteredBody}/>
-    <Post author="Caaminee" body="You need to come to NYC to have a life"/>
-    </ul>
+    {isPosting ?  
+      <Modal onClose={onStopPosting}>
+        <NewPost 
+          onBodyChange={bodyChangeHandler} 
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal> : null}
+      <ul className={classes.posts}>
+      <Post author={enteredAuthor} body={enteredBody}/>
+      <Post author="Meelyn" body="Learning React JS is fun!"/>
+      </ul>
     </>
   )
 }
